da47c22b72e5294621ee42cf81b76783e68c4191
Tateishi_Yudai <yudai_t@sc092.(none)>
2016-10-19 14:18:38 +0900

    scripts for shirokane3

---
+++ fastq_to_bam.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+#$ -S /bin/sh
+
+if [ $# -lt 4 ]
+then
+    echo "Usage:./fastq_to_bam.sh reference _1.fastq _2.fastq split"
+    exit 1
+fi
+
+filename1=${2##*/}
+filename2=${3##*/}
+sequence_name=${filename1%_*}
+
+read=`grep "@$sequence_name" $2 | wc -l`
+sp=`expr \(\($read\/$4\)+1\)\*4`
+mkdir split
+cd split
+split -d -l $(($sp)) ../$2 $filename1.
+echo "$2 split"
+split -d -l $(($sp)) ../$3 $filename2.
+echo "$3 split"
+cd ..
+
+qsub -o ./qlogs -e ./qlogs -cwd  -t 1-$4:1 ./make_sam.sh $1 ./split/$sequence_name
+
+while [ $((`qstat | wc -l`-3)) -gt 1 ]
+do
+	sleep 1m
+done
+
+samtools merge $sequence_name.bam ./split/$seqence_name.*.bam
+samtools sort -o $sequence_name.sorted.bam $sequence_name.bam
+samtools index $sequence_name.sorted.bam
+
---
+++ make_sam.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#$ -S /bin/sh
+
+
+SGE=`expr $SGE_TASK_ID - 1`
+if [ $SGE -lt 10 ]
+then
+	taskid=0$SGE
+else
+	taskid=$SGE
+fi
+
+bwa aln $1 $2_1.fastq.$taskid > $2_1.$taskid.sai
+bwa aln $1 $2_2.fastq.$taskid > $2_2.$taskid.sai
+
+bwa sampe $1 $2_1.$taskid.sai. $2_2.$taskid.sai $2_1.fastq.$taskid $2_2.fastq.$taskid > $2.$taskid.sam
+
+samtools view -bS $2.$taskid.sam > $2.$taskid.bam
+
+echo "make $2.$taskid.bam."
+
